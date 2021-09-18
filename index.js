7963e70f47fc6aa612cb096e9622056ac6fd9a03
ArlBiern <arielbiernacki@gmail.com>
2021-09-18 20:04:33 +0200

    Add fetch from json file

--- index.js
+++ index.js
@@ -1,21 +1,45 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const btn = document.querySelector('.getText');
+  const btnText = document.querySelector('.getText');
+  const btnUsers = document.querySelector('.getUsers');
+  let textLoaded = false;
+  let usersLoaded = false;
 
   const getText = () => {
-    fetch('sampe.txt')
+    fetch('sample.txt')
       .then(res => {
         if (res.ok) {
-          return res.text()
+          btnText.removeEventListener('click', getText);
+          return res.text();
         } else {
           return Promise.reject(`Something went wrong: ${res.status} - ${res.statusText}`)
         }
       })
       .then( text => {
-        document.querySelector('.txt_cnt').innerText = text
+        document.querySelector('.txt').innerText = text
       })
       .catch(err => alert(err));
   }
 
-  btn.addEventListener('click', getText);
+  const getUsers = () => {
+    fetch('users.json')
+      .then(res => {
+        if (res.ok) {
+          btnUsers.removeEventListener('click', getUsers);
+          return res.json()
+        } else {
+          return Promise.reject(`Something went wrong: ${res.status} - ${res.statusText}`)
+        }
+      })
+      .then( data => {
+        let list = document.querySelector('.users');
+        data.forEach(el => {
+          list.innerHTML += `<li>Name: ${el.name}, email: ${el.email}</li>`
+        });
+      })
+      .catch(err => alert(err));
+  }
+
+  btnText.addEventListener('click', getText);
+  btnUsers.addEventListener('click', getUsers);
 
 });
